d2d1607ca39330ddb7254f0eff3ce614434d4421
Michal Trabski <michal.trabski@gmail.com>
2019-10-17 14:15:14 +0200

    added timer to Lesson2 using useEffect hook

--- src/pages/Lesson2/Lesson2.js
+++ src/pages/Lesson2/Lesson2.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Lesson2.css";
+import styled, { css } from "styled-components";
+
+const Hand = styled.div`
+  transform: rotate(-90deg);
+  transform-origin: 0%;
+  transition-timing-function: ease;
+  background-color: black;
+  width: 60%;
+  height: 6px;
+  background-color: black;
+
+  ${({ second }) =>
+    second &&
+    css`
+      background-color: blue;
+      width: 90%;
+      height: 1px;
+    `};
+
+  ${({ minute }) =>
+    minute &&
+    css`
+      width: 80%;
+      height: 4px;
+    `};
+`;
+
+const calculateRotation = (time, number = 60) =>
+  `rotate(${(time / number) * 360 - 90}deg)`;
 
 const Lesson2 = ({ lesson }) => {
-  return "Komponent Lesson2";
+  console.log("Lesson2");
+  const [time, setTime] = useState({ s: 0, m: 0, h: 0 });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("This will run every second!");
+      let d = new Date();
+      let s = d.getSeconds();
+      let m = d.getMinutes();
+      let h = d.getHours();
+      setTime({ s, m, h });
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [time]);
+
+  const { s, m, h } = time;
+
+  return (
+    <>
+      {console.log("Lesson2 return ")}
+      {`s=${s}m=${m}h=${h}`}
+      <h2 className="text-center">lesson 2</h2>
+      <div className="clock">
+        <div className="hand-wrapper">
+          <Hand second style={{ transform: calculateRotation(s) }} />
+          <Hand minute style={{ transform: calculateRotation(m) }} />
+          <Hand style={{ transform: calculateRotation(h, 12) }} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Lesson2;
--- src/pages/Lesson2/rawjs/js.js
+++ src/pages/Lesson2/rawjs/js.js
@@ -1,20 +1,16 @@
-const calculation = (item, nr) => {
-  return (item / nr) * 360 - 90;
+const roteteClockHand = (time, className, nr = 60) => {
+  let x = (time / nr) * 360 - 90;
+  document.querySelector(className).style.transform = `rotate(${x}deg)`;
 };
-const makeRotation = (second, className, nr = 60) => {
-  let secondOnWatch = calculation(second, nr);
-  let secHand = document.querySelector(className);
-  secHand.style.transform = `rotate(${secondOnWatch}deg)`;
-};
-const setDate = () => {
+const setWatch = () => {
   let d = new Date();
   let second = d.getSeconds();
   let minute = d.getMinutes();
   let hour = d.getHours();
 
-  makeRotation(second, ".second-hand");
-  makeRotation(minute, ".minute-hand");
-  makeRotation(hour, ".hour-hand", 12);
+  roteteClockHand(second, ".second-hand");
+  roteteClockHand(minute, ".minute-hand");
+  roteteClockHand(hour, ".hour-hand", 12);
 };
-setDate();
-const interval = setInterval(setDate, 1000);
+setWatch();
+setInterval(setWatch, 1000);
